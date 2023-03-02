e2909eebc83a491b63681e7ba7a72dc32ae09f6c
edalzell <erin@silentz.co>
2023-03-02 09:42:32 -0800

    not a method

--- src/Forma.php
+++ src/Forma.php
@@ -5,7 +5,6 @@ namespace Edalzell\Forma;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static mixed methodName()
  * @method static void add(string $package, string $controller = null)
  * @method static \Edalzell\Forma\Addon findBySlug(string $slug)
  * @method static \Illuminate\Support\Collection all()
