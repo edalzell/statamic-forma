1d26f70faeb99b5b28f54d06db5616f87362c6fe
Erin Dalzell <erin@silentz.co>
2023-03-02 09:43:17 -0800

    Proteus 2.0 (#11)

--- src/Forma.php
+++ src/Forma.php
@@ -4,6 +4,11 @@ namespace Edalzell\Forma;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static void add(string $package, string $controller = null)
+ * @method static \Edalzell\Forma\Addon findBySlug(string $slug)
+ * @method static \Illuminate\Support\Collection all()
+ */
 class Forma extends Facade
 {
     protected static function getFacadeAccessor()
