a90db74e8029f9062e231dcc814b9c4ef130a818
edalzell <erin@silentz.co>
2023-06-05 19:40:07 -0700

    wip

--- src/Addons.php
+++ src/Addons.php
@@ -4,24 +4,32 @@ namespace Edalzell\Forma;
 
 use Illuminate\Support\Collection;
 use Statamic\Extend\Addon;
-use Statamic\Facades\Addon as AddonAPI;
+use Statamic\Facades\Addon as AddonFacade;
 
 class Addons
 {
     private array $addons = [];
 
-    public function add(string $package, string $controller = null)
+    public function add(string $package, string $controller = null, string $handle = null)
     {
-        $this->addons[$package] = $controller;
+        $this->addons[$package] = [
+            'controller' => $controller,
+            'handle' => $handle,
+        ];
     }
 
     public function findBySlug(string $slug): Addon
     {
-        return AddonAPI::all()->first(fn ($addon) => $addon->slug() === $slug);
+        dd($this->addons);
+        return AddonFacade::all()->first(fn ($addon) => $addon->slug() === $slug);
     }
 
     public function all(): Collection
     {
-        return collect($this->addons)->map(fn ($controller, $package) => new FormaAddon($package, $controller));
+        return collect($this->addons)
+            ->map(fn ($config, $package) => new FormaAddon(
+                $package,
+                ...$config
+            ));
     }
 }
--- src/FormaAddon.php
+++ src/FormaAddon.php
@@ -5,20 +5,24 @@ namespace Edalzell\Forma;
 use Illuminate\Support\Facades\Route;
 use Statamic\CP\Navigation\Nav;
 use Statamic\Extend\Addon;
-use Statamic\Facades\Addon as AddonAPI;
+use Statamic\Facades\Addon as AddonFacade;
 use Statamic\Facades\Blink;
-use Statamic\Facades\CP\Nav as NavAPI;
+use Statamic\Facades\CP\Nav as NavFacade;
 use Statamic\Statamic;
 
 class FormaAddon
 {
     private string $addon;
+
     private string $controller;
 
-    public function __construct(string $package, ?string $controller)
+    private ?string $handle;
+
+    public function __construct(string $package, ?string $controller = null, ?string $handle = null)
     {
         $this->addon = $package;
         $this->controller = $controller ?: ConfigController::class;
+        $this->handle = $handle;
     }
 
     public function boot()
@@ -33,26 +37,31 @@ class FormaAddon
             return;
         }
 
-        NavAPI::extend(fn (Nav $nav) => $nav
+        NavFacade::extend(fn (Nav $nav) => $nav
             ->content($addon->name())
             ->section('Settings')
             ->can('manage addon settings')
-            ->route($addon->slug() . '.config.edit')
+            ->route($this->handle().'.config.edit')
             ->icon('settings-horizontal'));
     }
 
     private function getAddon(): ?Addon
     {
-        return Blink::once($this->addon, fn () => AddonAPI::get($this->addon));
+        return Blink::once($this->addon, fn () => AddonFacade::get($this->addon));
+    }
+
+    private function handle(): ?string
+    {
+        return $this->handle ?? $this->getAddon()->slug();
     }
 
     private function registerRoutes()
     {
-        if (! $addon = $this->getAddon()) {
+        if (! $handle = $this->handle()) {
             return;
         }
 
-        Statamic::pushCpRoutes(fn () => Route::name($addon->slug())->prefix($addon->slug())->group(function () {
+        Statamic::pushCpRoutes(fn () => Route::name($handle)->prefix($handle)->group(function () {
             Route::name('.config.')->prefix('config')->group(function () {
                 Route::get('edit', [$this->controller, 'edit'])->name('edit');
                 Route::post('update', [$this->controller, 'update'])->name('update');
