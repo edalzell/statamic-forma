4a78f660bc29d8166f93c548e9979728460ce5db
Erin Dalzell <erin@silentz.co>
2025-03-01 13:02:32 -0800

    Discard changes to src/Events/ConfigSaved.php

--- src/Events/ConfigSaved.php
+++ src/Events/ConfigSaved.php
@@ -3,11 +3,14 @@
 namespace Edalzell\Forma\Events;
 
 use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
 use Statamic\Extend\Addon;
 
 class ConfigSaved
 {
-    use Dispatchable;
+    use Dispatchable, SerializesModels;
 
-    public function __construct(public array $config, public Addon $addon) {}
+    public function __construct(public array $config, public Addon $addon)
+    {
+    }
 }
