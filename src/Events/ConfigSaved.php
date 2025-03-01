49fe9527339eac2fa3c70271c30e0f811768a8dc
edalzell <erin@silentz.co>
2025-03-01 13:00:06 -0800

    not needed

--- src/Events/ConfigSaved.php
+++ src/Events/ConfigSaved.php
@@ -3,14 +3,11 @@
 namespace Edalzell\Forma\Events;
 
 use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Queue\SerializesModels;
 use Statamic\Extend\Addon;
 
 class ConfigSaved
 {
-    use Dispatchable, SerializesModels;
+    use Dispatchable;
 
-    public function __construct(public array $config, public Addon $addon)
-    {
-    }
+    public function __construct(public array $config, public Addon $addon) {}
 }
